5d2e519a065cf189545b32ba9a46a9836024cfbd
Shayki Abramczyk <shayki5@gmail.com>
2019-07-28 09:27:11 +0300

    Update createPullRequest.ps1

--- task/createPullRequest.ps1
+++ task/createPullRequest.ps1
@@ -1,67 +1,71 @@
-[CmdletBinding()]
-param(
-   [string] $sourceBranch,
-   [string] $targetBranch,
-   [string] $title,
-   [string] $description,
-   [string] $reviewers
-)
+function RunTask
+{
+   [CmdletBinding()]
+   param(
+      [string] $sourceBranch,
+      [string] $targetBranch,
+      [string] $title,
+      [string] $description,
+      [string] $reviewers
+   )
 
-Trace-VstsEnteringInvocation $MyInvocation
-try {
-    # Get inputs
-    $sourceBranch = Get-VstsInput -Name 'sourceBranch' -Require
-    $targetBranch = Get-VstsInput -Name 'targetBranch' -Require
-    $title = Get-VstsInput -Name 'title' -Require
-    $description = Get-VstsInput -Name 'description' 
-    $reviewers = Get-VstsInput -Name 'reviewers'
+   Trace-VstsEnteringInvocation $MyInvocation
+   try {
+       # Get inputs
+       $sourceBranch = Get-VstsInput -Name 'sourceBranch' -Require
+       $targetBranch = Get-VstsInput -Name 'targetBranch' -Require
+       $title = Get-VstsInput -Name 'title' -Require
+       $description = Get-VstsInput -Name 'description' 
+       $reviewers = Get-VstsInput -Name 'reviewers'
 
-    if(!$sourceBranch.Contains("refs"))
-    {
-        $sourceBranch = "refs/heads/$sourceBranch"
-    }
-    $targetBranch = "refs/heads/$targetBranch"
-    if($reviewers -ne "")
-    {
-        Write-Host "not null"
-        
-         $url = "$($env:System_TeamFoundationCollectionUri)_apis/userentitlements?api-version=5.0-preview.2" 
-         $url = $url.Replace("//dev","//vsaex.dev")
-         $head = @{ Authorization = "Bearer $env:System_AccessToken" }
-         $users = Invoke-RestMethod -Uri $url -Method Get -ContentType application/json -Headers $head
+       if(!$sourceBranch.Contains("refs"))
+       {
+           $sourceBranch = "refs/heads/$sourceBranch"
+       }
+       $targetBranch = "refs/heads/$targetBranch"
+       if($reviewers -ne "")
+       {
+           Write-Host "not null"
 
-         $reviewers = $reviewers.Split(';')
-         $usersId = @()
-         ForEach($reviewer in $reviewers)
-         {
-              $userId = $users.items.Where({ $_.user.mailAddress -eq $reviewer }).id
-              $usersId += @{ id = "$userId" }
-         }
+            $url = "$($env:System_TeamFoundationCollectionUri)_apis/userentitlements?api-version=5.0-preview.2" 
+            $url = $url.Replace("//dev","//vsaex.dev")
+            $head = @{ Authorization = "Bearer $env:System_AccessToken" }
+            $users = Invoke-RestMethod -Uri $url -Method Get -ContentType application/json -Headers $head
 
-         $body = @{
-             sourceRefName = "$sourceBranch"
-             targetRefName = "$targetBranch"
-             title = "$title"
-             description = "$description"
-             reviewers = @( $usersId )
-         }
-         CreatePullRequest -body $body -reviewers $reviewers
-    }
+            $reviewers = $reviewers.Split(';')
+            $usersId = @()
+            ForEach($reviewer in $reviewers)
+            {
+                 $userId = $users.items.Where({ $_.user.mailAddress -eq $reviewer }).id
+                 $usersId += @{ id = "$userId" }
+            }
 
-    else
-    {
-        $body = @{
-            sourceRefName = "$sourceBranch"
-            targetRefName = "$targetBranch"
-            title = "$title"
-            description = "$description"
-        }
-        CreatePullRequest -body $body
-    }
+            $body = @{
+                sourceRefName = "$sourceBranch"
+                targetRefName = "$targetBranch"
+                title = "$title"
+                description = "$description"
+                reviewers = @( $usersId )
+            }
+            CreatePullRequest -body $body -reviewers $reviewers
+       }
+
+       else
+       {
+           $body = @{
+               sourceRefName = "$sourceBranch"
+               targetRefName = "$targetBranch"
+               title = "$title"
+               description = "$description"
+           }
+           CreatePullRequest -body $body
+       }
 
 
-} finally {
-    Trace-VstsLeavingInvocation $MyInvocation
+   } finally {
+       Trace-VstsLeavingInvocation $MyInvocation
+   }
+
 }
 
 function CreatePullRequest($body, $reviewers)
@@ -87,5 +91,6 @@ function CreatePullRequest($body, $reviewers)
         Write-Host "Pull Request $($response.pullRequestId) created."
 
     }
-
 }
+
+RunTask
