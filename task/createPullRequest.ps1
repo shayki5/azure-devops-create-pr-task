4f5185de54f1a026636d8a0e7080cdebcc3aa54e
Shayki Abramczyk <shayki5@gmail.com>
2019-10-06 12:39:17 +0300

    test reviewr

--- task/createPullRequest.ps1
+++ task/createPullRequest.ps1
@@ -33,7 +33,7 @@ function RunTask
            } 
        }
 
-       # If the target branch is like feature/*
+       # If is multi-target branch, like feature/*
        else
        {
            Set-Location $env:Build_SourcesDirectory
@@ -42,8 +42,15 @@ function RunTask
                 if($_ -match ($targetBranch.Split('/')[0]))
                 {
                         $newTargetBranch = $_.Split('/')[2] + "/" + $_.Split('/')[3]
-                        $newTargetBranch = "refs/heads/$newTargetBranch"
-                        CheckReviewersAndCreatePR -sourceBranch $sourceBranch -targetBranch $newTargetBranch -title $title -description $description -reviewers $reviewers
+                        $newTargetBranch = "$newTargetBranch"
+                        if($repoType -eq "Azure DevOps")
+                        {  
+                            CheckReviewersAndCreatePR -sourceBranch $sourceBranch -targetBranch $newTargetBranch -title $title -description $description -reviewers $reviewers    
+                        }
+                        else # Is GitHub repository 
+                        {
+                            CreateGitHubPullRequest -sourceBranch $sourceBranch -targetBranch $newTargetBranch -title $title -description $description
+                        }
                 }
            })
        }
@@ -51,6 +58,46 @@ function RunTask
    } finally {
        Trace-VstsLeavingInvocation $MyInvocation
    }
+}
+
+function CheckReviewersAndCreatePR($sourceBranch, $targetBranch, $title, $description, $reviewers)
+{
+    $sourceBranch = "refs/heads/$sourceBranch"
+    $targetBranch = "refs/heads/$targetBranch"    
+    if($reviewers -ne "")
+    {
+        $url = "$($env:System_TeamFoundationCollectionUri)_apis/userentitlements?api-version=5.0-preview.2"
+        $url = $url.Replace("//dev","//vsaex.dev")
+        Write-Debug $url
+        $head = @{ Authorization = "Bearer $env:System_AccessToken" }
+        $users = Invoke-RestMethod -Uri $url -Method Get -ContentType application/json -Headers $head
+        $reviewers = $reviewers.Split(';')
+        $usersId = @()
+        ForEach($reviewer in $reviewers)
+        {
+              $userId = $users.items.Where({ $_.user.mailAddress -eq $reviewer }).id
+              $usersId += @{ id = "$userId" }
+        }
+        $body = @{
+            sourceRefName = "$sourceBranch"
+            targetRefName = "$targetBranch"
+            title = "$title"
+            description = "$description"
+            reviewers = @( $usersId )
+        }
+        CreatePullRequest -body $body -reviewers $reviewers
+
+    }
+    else
+    {
+        $body = @{
+            sourceRefName = "$sourceBranch"
+            targetRefName = "$targetBranch"
+            title = "$title"
+            description = "$description"
+        }
+        CreatePullRequest -body $body
+    }
 }
 
 function CreateGitHubPullRequest($sourceBranch, $targetBranch, $title, $description)
@@ -83,12 +130,28 @@ function CreateGitHubPullRequest($sourceBranch, $targetBranch, $title, $descript
         base = "$targetBranch"
         title = "$title"
         body = "$description"
+        requested_reviewers = @( login = "shayki-test" )
     }
     $jsonBody = ConvertTo-Json $body
     Write-Debug $jsonBody
 
     $header = @{Authorization=("token $token")}
-    Invoke-RestMethod -Uri $url -Method Post -ContentType application/json -Headers $header -Body $jsonBody
+    try 
+    {
+        $response =  Invoke-RestMethod -Uri $url -Method Post -ContentType application/json -Headers $header -Body $jsonBody
+        if($response -ne $Null) # If the response not null - the create PR succeeded
+        {
+            Write-Host "*************************"
+            Write-Host "******** Success ********"
+            Write-Host "*************************"
+            Write-Host "Pull Request $($response.number) created."
+        }
+    }
+    catch 
+    {
+        Write-Error $_
+        Write-Error $_.Exception.Message
+    }
 }
 function CreatePullRequest($body, $reviewers)
 {
@@ -132,46 +195,6 @@ function CreatePullRequest($body, $reviewers)
             Write-Error $_
             Write-Error $_.Exception.Message
         }
-    }
-}
-
-function CheckReviewersAndCreatePR($sourceBranch, $targetBranch, $title, $description, $reviewers)
-{
-    $sourceBranch = "refs/heads/$sourceBranch"
-    $targetBranch = "refs/heads/$targetBranch"    
-    if($reviewers -ne "")
-    {
-        $url = "$($env:System_TeamFoundationCollectionUri)_apis/userentitlements?api-version=5.0-preview.2"
-        $url = $url.Replace("//dev","//vsaex.dev")
-        Write-Debug $url
-        $head = @{ Authorization = "Bearer $env:System_AccessToken" }
-        $users = Invoke-RestMethod -Uri $url -Method Get -ContentType application/json -Headers $head
-        $reviewers = $reviewers.Split(';')
-        $usersId = @()
-        ForEach($reviewer in $reviewers)
-        {
-              $userId = $users.items.Where({ $_.user.mailAddress -eq $reviewer }).id
-              $usersId += @{ id = "$userId" }
-        }
-        $body = @{
-            sourceRefName = "$sourceBranch"
-            targetRefName = "$targetBranch"
-            title = "$title"
-            description = "$description"
-            reviewers = @( $usersId )
-        }
-        CreatePullRequest -body $body -reviewers $reviewers
-
-    }
-    else
-    {
-        $body = @{
-            sourceRefName = "$sourceBranch"
-            targetRefName = "$targetBranch"
-            title = "$title"
-            description = "$description"
-        }
-        CreatePullRequest -body $body
     }
 }
 
