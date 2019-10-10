ce8bb53a94df77dc109c8e4db5b03fb007c23e28
Shayki Abramczyk <shayki5@gmail.com>
2019-10-10 10:30:17 +0300

    Refactor the code

--- task/createPullRequest.ps1
+++ task/createPullRequest.ps1
@@ -1,16 +1,18 @@
 function RunTask
 {
    [CmdletBinding()]
-   param(
-      [string] $sourceBranch,
-      [string] $targetBranch,
-      [string] $title,
-      [string] $description,
-      [string] $reviewers
+   Param
+   (
+      [string]$sourceBranch,
+      [string]$targetBranch,
+      [string]$title,
+      [string]$description,
+      [string]$reviewers
    )
 
    Trace-VstsEnteringInvocation $MyInvocation
-   try {
+   try
+   {
        # Get inputs
        $sourceBranch = Get-VstsInput -Name 'sourceBranch' -Require
        $targetBranch = Get-VstsInput -Name 'targetBranch' -Require
@@ -18,19 +20,11 @@ function RunTask
        $description = Get-VstsInput -Name 'description'
        $reviewers = Get-VstsInput -Name 'reviewers'
        $repoType = Get-VstsInput -Name 'repoType' -Require
-       
-
+      
        # If the target branch is only one branch
        if(!$targetBranch.Contains('*'))
        {
-           if($repoType -eq "Azure DevOps")
-           {  
-               CheckReviewersAndCreatePR -sourceBranch $sourceBranch -targetBranch $targetBranch -title $title -description $description -reviewers $reviewers    
-           }
-           else # Is GitHub repository 
-           {
-               CreateGitHubPullRequest -sourceBranch $sourceBranch -targetBranch $targetBranch -title $title -description $description -reviewers $reviewers
-           } 
+          CreatePullRequest -sourceBranch $sourceBranch -targetBranch $targetBranch -title $title -description $description -reviewers $reviewers -repoType $repoTyoe 
        }
 
        # If is multi-target branch, like feature/*
@@ -41,78 +35,64 @@ function RunTask
            $branches.ForEach({
                 if($_ -match ($targetBranch.Split('/')[0]))
                 {
-                        $newTargetBranch = $_.Split('/')[2] + "/" + $_.Split('/')[3]
-                        $newTargetBranch = "$newTargetBranch"
-                        if($repoType -eq "Azure DevOps")
-                        {  
-                            CheckReviewersAndCreatePR -sourceBranch $sourceBranch -targetBranch $newTargetBranch -title $title -description $description -reviewers $reviewers    
-                        }
-                        else # Is GitHub repository 
-                        {
-                            CreateGitHubPullRequest -sourceBranch $sourceBranch -targetBranch $newTargetBranch -title $title -description $description -reviewers $reviewers
-                        }
+                    $newTargetBranch = $_.Split('/')[2] + "/" + $_.Split('/')[3]
+                    $newTargetBranch = "$newTargetBranch"
+                    CreatePullRequest -sourceBranch $sourceBranch -targetBranch $newTargetBranch -title $title -description $description -reviewers $reviewers -repoType $repoTyoe 
                 }
            })
        }
-
-   } finally {
-       Trace-VstsLeavingInvocation $MyInvocation
    }
+
+   finally
+   {
+       Trace-VstsLeavingInvocation $MyInvocation
+   
 }
 
-function CheckReviewersAndCreatePR($sourceBranch, $targetBranch, $title, $description, $reviewers)
+function CreatePullRequest()
 {
-    if(!$sourceBranch.Contains("refs")
-    {
-        $sourceBranch = "refs/heads/$sourceBranch"
-    }
-    $targetBranch = "refs/heads/$targetBranch"    
-    if($reviewers -ne "")
-    {
-        $url = "$($env:System_TeamFoundationCollectionUri)_apis/userentitlements?api-version=5.0-preview.2"
-        $url = $url.Replace("//dev","//vsaex.dev")
-        Write-Debug $url
-        $head = @{ Authorization = "Bearer $env:System_AccessToken" }
-        $users = Invoke-RestMethod -Uri $url -Method Get -ContentType application/json -Headers $head
-        $reviewers = $reviewers.Split(';')
-        $usersId = @()
-        ForEach($reviewer in $reviewers)
-        {
-              $userId = $users.items.Where({ $_.user.mailAddress -eq $reviewer }).id
-              $usersId += @{ id = "$userId" }
-        }
-        $body = @{
-            sourceRefName = "$sourceBranch"
-            targetRefName = "$targetBranch"
-            title = "$title"
-            description = "$description"
-            reviewers = @( $usersId )
-        }
-        CreatePullRequest -body $body -reviewers $reviewers
 
+    [CmdletBinding()]
+    Param
+    (
+       [string]$repoType,
+       [string]$sourceBranch,
+       [string]$targetBranch,
+       [string]$title,
+       [string]$description,
+       [string]$reviewers
+    )
+
+    if($repoType -eq "Azure DevOps")
+    { 
+        CreateAzureDevOpsPullRequest -sourceBranch $sourceBranch -targetBranch $targetBranch -title $title -description $description -reviewers $reviewers 
     }
-    else
+
+    else # Is GitHub repository
     {
-        $body = @{
-            sourceRefName = "$sourceBranch"
-            targetRefName = "$targetBranch"
-            title = "$title"
-            description = "$description"
-        }
-        CreatePullRequest -body $body
+        CreateGitHubPullRequest -sourceBranch $sourceBranch -targetBranch $targetBranch -title $title -description $description -reviewers $reviewers
     }
 }
 
-function CreateGitHubPullRequest($sourceBranch, $targetBranch, $title, $description, $reviewers)
+function CreateGitHubPullRequest()
 {
+    [CmdletBinding()]
+    Param
+    (
+       [string]$repoType,
+       [string]$sourceBranch,
+       [string]$targetBranch,
+       [string]$title,
+       [string]$description,
+       [string]$reviewers
+    )
+
     Write-Host "The source branch is: $sourceBranch"
     Write-Host "The target branch is: $targetBranch"
     Write-Host "The title is: $title"
     Write-Host "The description is: $description"
-
     $serviceNameInput = Get-VstsInput -Name ConnectedServiceNameSelector -Default 'githubEndpoint'
     $serviceName = Get-VstsInput -Name $serviceNameInput -Default (Get-VstsInput -Name DeploymentEnvironmentName)
-
     if (!$serviceName) {
         # Let the task SDK throw an error message if the input isn't defined.
         Get-VstsInput -Name $serviceNameInput -Require
@@ -120,23 +100,21 @@ function CreateGitHubPullRequest($sourceBranch, $targetBranch, $title, $descript
 
     $endpoint = Get-VstsEndpoint -Name $serviceName -Require
     $token = $endpoint.Auth.Parameters.accessToken
-    
     $repoUrl = $env:BUILD_REPOSITORY_URI
     $owner = $repoUrl.Split('/')[3]
     $repo = $repoUrl.Split('/')[4]
     $url = "https://api.github.com/repos/$owner/$repo/pulls"
-
     $body = @{
         head = "$sourceBranch"
         base = "$targetBranch"
         title = "$title"
         body = "$description"
     }
+
     $jsonBody = ConvertTo-Json $body
     Write-Debug $jsonBody
-
     $header = @{Authorization=("token $token")}
-    try 
+    try
     {
         $response =  Invoke-RestMethod -Uri $url -Method Post -ContentType application/json -Headers $header -Body $jsonBody
         if($response -ne $Null) # If the response not null - the create PR succeeded
@@ -145,21 +123,30 @@ function CreateGitHubPullRequest($sourceBranch, $targetBranch, $title, $descript
             Write-Host "******** Success ********"
             Write-Host "*************************"
             Write-Host "Pull Request $($response.number) created."
+            # If the reviewers not null so add the reviewers to the PR
             if($reviewers -ne "")
             {
                 CreateGitHubReviewers -reviewers $reviewers -token $token -prNumber $response.number
             }
         }
     }
-    catch 
+
+    catch
     {
         Write-Error $_
         Write-Error $_.Exception.Message
     }
 }
 
-function CreateGitHubReviewers($reviewers, $token, $prNumber)
+function CreateGitHubReviewers()
 {
+    [CmdletBinding()]
+    Param
+    (
+       [string]$reviewers,
+       [string]$token,
+       [string]$prNumber
+    )
     $reviewers = $reviewers.Split(';')
     $repoUrl = $env:BUILD_REPOSITORY_URI
     $owner = $repoUrl.Split('/')[3]
@@ -175,7 +162,7 @@ function CreateGitHubReviewers($reviewers, $token, $prNumber)
     $jsonBody = $body | ConvertTo-Json
     Write-Debug $jsonBody
     $header = @{Authorization=("token $token")}
-    try 
+    try
     {
         Write-Host "Add reviewers the the Pull Request..."
         $response =  Invoke-RestMethod -Uri $url -Method Post -ContentType application/json -Headers $header -Body $jsonBody
@@ -185,28 +172,57 @@ function CreateGitHubReviewers($reviewers, $token, $prNumber)
             Write-Host "Reviewrs area ddedd to PR #$prNumber"
         }
     }
-    catch 
+
+    catch
     {
         Write-Error $_
         Write-Error $_.Exception.Message
     }
 }
-function CreatePullRequest($body, $reviewers)
+
+function CreateAzureDevOpsPullRequest()
 {
+    [CmdletBinding()]
+    Param
+    (
+       [string]$sourceBranch,
+       [string]$targetBranch,
+       [string]$title,
+       [string]$description,
+       [string]$reviewers
+    )
+    if(!$sourceBranch.Contains("refs"))
+    {
+        $sourceBranch = "refs/heads/$sourceBranch"
+    }
+
+    $targetBranch = "refs/heads/$targetBranch"  
     Write-Host "The source branch is: $($body.sourceRefName)"
     Write-Host "The target branch is: $($body.targetRefName)"
     Write-Host "The title is: $($body.title)"
     Write-Host "The description is: $($body.description)"
-    if($body.Keys -contains "reviewers")
+    $body = @{
+        sourceRefName = "$sourceBranch"
+        targetRefName = "$targetBranch"
+        title = "$title"
+        description = "$description"
+        reviewers = ""
+    }
+
+    if($reviewers -ne "")
     {
+        $usersId = GetUsersId -reviewers $reviewers
+        $body.reviewers = @( $usersId )
         Write-Host "The reviewers are: $($reviewers.Split(';'))"
     }
+
     $head = @{ Authorization = "Bearer $env:System_AccessToken" }
     $jsonBody = ConvertTo-Json $body
     Write-Debug $jsonBody
     $url = "$env:System_TeamFoundationCollectionUri$env:System_TeamProject/_apis/git/repositories/$env:Build_Repository_Name/pullrequests?api-version=5.0"
     Write-Debug $url
-    try 
+
+    try
     {
         $response =  Invoke-RestMethod -Uri $url -Method Post -Headers $head -Body $jsonBody -ContentType application/json
         if($response -ne $Null) # If the response not null - the create PR succeeded
@@ -217,23 +233,46 @@ function CreatePullRequest($body, $reviewers)
             Write-Host "Pull Request $($response.pullRequestId) created."
         }
     }
-    catch 
+
+    catch
     {
-        try { $errorMessage = ($_ | ConvertFrom-Json).message }
-        catch { }
         # If the error contains TF401179 it's mean that there is alredy a PR for the branches, so I display a warning
-        if($errorMessage -match "TF401179") 
+        if($errorMessage -match "TF401179")
         {
-            Write-Warning $errorMessage
+            Write-Warning $_
         }
+
         else # If there is an error - fail the task
         {
-            try { Write-Warning $errorMessage }
-            catch { }
             Write-Error $_
             Write-Error $_.Exception.Message
         }
     }
+}
+
+function GetUsersId()
+
+{
+    [CmdletBinding()]
+    Param
+    (
+       [string]$reviewers
+    )
+
+    $url = "$($env:System_TeamFoundationCollectionUri)_apis/userentitlements?api-version=5.0-preview.2"
+    $url = $url.Replace("//dev","//vsaex.dev")
+    Write-Debug $url
+    $head = @{ Authorization = "Bearer $env:System_AccessToken" }
+    $users = Invoke-RestMethod -Uri $url -Method Get -ContentType application/json -Headers $head
+    $reviewers = $reviewers.Split(';')
+    $usersId = @()
+    ForEach($reviewer in $reviewers)
+    {
+        $userId = $users.items.Where({ $_.user.mailAddress -eq $reviewer }).id
+        $usersId += @{ id = "$userId" }
+
+    }
+    return $usersId
 }
 
 RunTask
