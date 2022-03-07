96f8080ad523604b20dd65a95a53ebfa4c9f9179
Shayki Abramczyk <shayki.abramczyk@tipalti.com>
2022-03-07 15:15:22 +0200

    fix delete url

--- task/createPullRequest.ps1
+++ task/createPullRequest.ps1
@@ -416,7 +416,7 @@ function GitHubAutoMerge {
     if($deleteSource)
     {
         Write-Host "Deleting the source branch..."
-        $url = "https://api.github.com/$owner/$repo/npm-demo/git/refs/heads/$sourceBranch"
+        $url = "https://api.github.com/$owner/$repo/git/refs/heads/$sourceBranch"
         try {
         Invoke-RestMethod -Uri $url -Method DELETE -ContentType application/json -Headers $header
         Write-Host "******** The branch $sourceBranch is deleted ********"
