5132c781e81a2610ea3a97a9d83a1a38db5e5225
Shayki Abramczyk <shayki.abramczyk@tipalti.com>
2022-03-09 12:03:29 +0200

    add prints

--- task/createPullRequest.ps1
+++ task/createPullRequest.ps1
@@ -296,10 +296,12 @@ function CreateGitHubReviewers() {
         [string]$prNumber,
         [string]$repo
     )
-    Write-Debug $reviewers
-    Write-Debug $reviewers.GetType()
+    Write-Debug "$reviewers"
+    Write-Debug "$reviewers.GetType()"
     $split = $reviewers.Split(';').Trim()
-    Write-Debug $split
+    Write-Debug "$split"
+    [array]$reviewers = $reviewers.Split(';').Trim()
+    Write-Debug "$reviewers"
     $repoUrl = $repo
     $owner = $repoUrl.Split('/')[0]
     $repo = $repoUrl.Split('/')[1]
